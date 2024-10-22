efe7784f300a22f263124309ce88addd3da63e11
webNerd69 <rroy64330@gmail.com>
2024-10-23 04:13:42 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Book {
+    // Book class definition will go here
+};
+
+class Library {
+    // Library class definition will go here
+};
+
+int main() {
+    cout << "Book Comparison Program" << endl;
+    
+    // Main program logic will go here
+    
+    return 0;
+}
