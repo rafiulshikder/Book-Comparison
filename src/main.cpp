6d0c6e912cfea35650aaf1c4327ee8aad8f5b43d
webNerd69 <rroy64330@gmail.com>
2024-11-03 01:02:07 +0530

    all the functionalities are added and the project is final and completed

--- src/main.cpp
+++ src/main.cpp
@@ -79,13 +79,22 @@ class BookComparison
 {
     map<string, list<string>> bookWords;
     map<string, map<string, int>> bookWordsCount;
-    map<string, map<string, float>> bookWordsNormalScore;
-    map<string, list<string>> bookMostUsedWords;
+    map<string, map<string, double>> bookWordsNormalScore;
+    map<string, list<string>> mostUsedWords;
+    map<string, double> bookSimilarities;
 
 public:
     void addBookWords(list<string> &bookNames = bookNames)
     {
-        set<string> excludeWords = {"a", "an", "the"};
+        cout<<"Books are being loaded..."<<endl;
+        set<string> excludeWords = {
+            "the", "be", "to", "of", "and", "a", "in", "that", "have", "I",
+            "it", "for", "not", "on", "with", "he", "as", "you", "do", "at",
+            "this", "but", "his", "by", "from", "they", "we", "say", "her",
+            "she", "or", "an", "will", "my", "one", "all", "would", "there",
+            "their", "what", "so", "up", "out", "if", "about", "who", "get",
+            "which", "go", "me"};
+
         for (const string &bookName : bookNames)
         {
             ifstream file(bookName);
@@ -107,52 +116,194 @@ public:
                 bookWords[bookName] = words;
                 file.close();
             }
-            // cout<<file.is_open()<<bookName<<endl;
-            // file.close();
         }
+        cout<<"All the bools have been added!"<<endl;
+        cout<<endl;
     }
     void addBookWordsCount()
     {
+        cout<<"Calculating word counts..."<<endl;
         for (const auto &book : bookWords)
         {
             map<string, int> wordCount;
             int totalWords = 0;
-            
+
             // Count frequency of each word
             for (const auto &word : book.second)
             {
                 wordCount[word]++;
                 totalWords++;
             }
-            
+
             // Add total word count
             wordCount["count"] = totalWords;
-            
+
             // Store in bookWordsCount map
             bookWordsCount[book.first] = wordCount;
         }
+        cout<<"Word counts have been calculated!"<<endl;
+        cout<<endl;
     }
-    void displayBookWords() {
-        for (const auto& book : bookWordsCount) {
-            cout << "Book: " << book.first << endl;
-            cout << "Word frequencies:" << endl;
-            for (const auto& word : book.second) {
-                if (word.first != "count") {
-                    cout << word.first << ": " << word.second << endl;
+    void calculateNormalScores()
+    {   
+        cout<<"Calculating normal scores of the words..."<<endl;
+        for (const auto &book : bookWordsCount)
+        {
+            map<string, double> normalScores;
+            int totalWords = book.second.at("count");
+
+            // Calculate normal score for each word
+            for (const auto &word : book.second)
+            {
+                if (word.first != "count")
+                {
+                    // Normal score = word frequency / total words
+                    normalScores[word.first] = static_cast<double>(word.second) / totalWords;
                 }
             }
-            cout << "Total words: " << book.second.at("count") << endl;
-            cout << endl;
+
+            // Store in bookWordsNormalScore map
+            bookWordsNormalScore[book.first] = normalScores;
         }
+        cout<<"Normal scores have been calculated!"<<endl;
+        cout<<endl;
+    }
+    void findMostUsedWords()
+    {   
+        cout<<"Finding the most used words..."<<endl;
+        for (const auto &book : bookWordsNormalScore)
+        {
+            // Create list of pairs to sort by normal score
+            list<pair<string, double>> wordScores;
+            for (const auto &word : book.second)
+            {
+                wordScores.push_back({word.first, word.second});
+            }
+
+            // Sort in descending order of normal scores
+            wordScores.sort(
+                [](const pair<string, double> &a, const pair<string, double> &b)
+                {
+                    return a.second > b.second;
+                });
+
+            // Take first 100 words (or less if book has fewer words)
+            list<string> topWords;
+            int count = 0;
+            int numWords = min(100, static_cast<int>(wordScores.size()));
+
+            for (const auto &wordScore : wordScores)
+            {
+                if (count >= numWords)
+                    break;
+                topWords.push_back(wordScore.first);
+                count++;
+            }
+
+            // Store in mostUsedWords map
+            mostUsedWords[book.first] = topWords;
+        }
+        cout<<"Most used words have been found!"<<endl;
+        cout<<endl;
+    }
+    void compareBooks()
+    {
+        cout<<"Comparing books..."<<endl;
+        cout << "\nBook Similarities:\n";
+        // Get list of all book names
+        vector<string> bookNames;
+        for (const auto &book : mostUsedWords) {
+            bookNames.push_back(book.first);
+        }
+        
+        // Compare each book with others (avoiding duplicates and self-comparisons)
+        for (size_t i = 0; i < bookNames.size(); i++) {
+            for (size_t j = i + 1; j < bookNames.size(); j++) {
+                string book1 = bookNames[i];
+                string book2 = bookNames[j];
+                
+                // Count matching words
+                int matchCount = 0;
+                for (const auto &word1 : mostUsedWords[book1]) {
+                    for (const auto &word2 : mostUsedWords[book2]) {
+                        if (word1 == word2) {
+                            matchCount++;
+                            break;
+                        }
+                    }
+                }
+                
+                // Calculate similarity index
+                double similarityIndex = static_cast<double>(matchCount) / 100.0;
+                
+                // Store in map with combined book names as key
+                string bookPairKey = book1 + " and " + book2;
+                bookSimilarities[bookPairKey] = similarityIndex;
+
+                cout<<bookPairKey<<" : "<<similarityIndex<<endl;
+            }
+        }
+        
+        // // Display results
+        // cout << "\nBook Similarities:\n";
+        // for (const auto &similarity : bookSimilarities) {
+        //     cout << similarity.first << ": " << similarity.second << endl;
+        // }
+
+        cout<<"All the books have been compared!"<<endl;
+        cout<<endl;
+    }
+    void findTopSimilarBooks() {
+        cout<<"Finding top 10 similar books..."<<endl;
+        // Create vector of pairs to sort similarities
+        vector<pair<string, double>> similarityPairs;
+        for (const auto &similarity : bookSimilarities) {
+            similarityPairs.push_back(similarity);
+        }
+        
+        // Sort by similarity value in descending order
+        sort(similarityPairs.begin(), similarityPairs.end(),
+            [](const pair<string, double> &a, const pair<string, double> &b) {
+                return a.second > b.second;
+            });
+            
+        // Get top 10 (or less if fewer pairs exist)
+        int numPairs = min(10, static_cast<int>(similarityPairs.size()));
+        
+        // Open file for writing
+        ofstream outFile("similar_Books.txt");
+        if (!outFile.is_open()) {
+            cout << "Error opening file for writing" << endl;
+            return;
+        }
+        
+        // Write and display top similar books
+        cout << "\nTop " << numPairs << " Most Similar Books:\n";
+        outFile << "Top " << numPairs << " Most Similar Books:\n";
+        
+        for (int i = 0; i < numPairs; i++) {
+            string output = to_string(i + 1) + ". " + similarityPairs[i].first + 
+                          " (Similarity: " + to_string(similarityPairs[i].second) + ")";
+            cout << output << endl;
+            outFile << output << endl;
+        }
+        
+        outFile.close();
+        cout<<"Most similar books have been found!"<<endl;
+        cout << "\nResults have been written to similar_Books.txt" << endl;
     }
 
 };
 
-int
-main()
+int main()
 {
     BookComparison book;
+    
     book.addBookWords();
     book.addBookWordsCount();
-    book.displayBookWords();
+    book.calculateNormalScores();
+    book.findMostUsedWords();
+    book.compareBooks();
+    book.findTopSimilarBooks();
+    // book.displayBookWords();
 }
